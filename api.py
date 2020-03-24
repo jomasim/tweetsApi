77bc5047fd5efb6114e1c1087835546adcb9955e
Jomasim <simjoms@gmail.com>
2020-03-24 14:14:48 +0300

    refactors code
    add `max` request param

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import make_response, jsonify
 from flask_restful import Resource, Api
 import GetOldTweets3 as got
@@ -8,8 +8,9 @@ api = Api(app)
 
 class Tweets(Resource):
     def get(self, username="@MOH_Kenya"):
+        max = request.args['max'] or 5
         tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
-                                           .setMaxTweets(10)\
+                                           .setMaxTweets(max)\
                                            .setTopTweets(True)\
                                            .setEmoji("unicode")
         tweets = got.manager.TweetManager.getTweets(tweetCriteria)
